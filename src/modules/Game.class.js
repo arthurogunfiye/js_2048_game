aad21ba638e2612832e2be0c0a30b379acf5e551
Arthur Ogunfuye <arthurity@yahoo.co.uk>
2025-05-08 07:44:41 +0100

    fix moveDown method

--- src/modules/Game.class.js
+++ src/modules/Game.class.js
@@ -115,7 +115,7 @@ class Game {
       return this.merge(this.compress(line.reverse()));
     });
 
-    this.state = this.transpose(transposed).reverse();
+    this.state = this.transpose(transposed);
 
     if (!this.areBoardMoves(prev, this.state)) {
       this.addRandomTile();
