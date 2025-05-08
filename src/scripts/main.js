2fa0f30b96fd597264ffc77291626f1ce795eb2e
Arthur Ogunfuye <arthurity@yahoo.co.uk>
2025-05-08 07:41:31 +0100

    PR changes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -30,7 +30,7 @@ function updateBoard() {
   game.getState().forEach((row) => {
     const newRow = document.createElement('tr');
 
-    newRow.classList.add('game-field');
+    newRow.classList.add('field-row');
 
     row.forEach((cell) => {
       const newCell = document.createElement('td');
